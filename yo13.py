4a67dca5dcccc9cf3b747fe794ceeacc4a92f89b
John Omernik <john@omernik.com>
2024-10-17 08:27:00 -0500

    Update yo13.py

--- yo13.py
+++ yo13.py
@@ -1,20 +1,23 @@
 import os
+import base64
 from cryptography.fernet import Fernet
 
 # Load the encryption key
-with open('encryption_key.key', 'rb') as key_file:
-    key = key_file.read()
+with open('o.txt', 'r') as key_file:
+    b64key = key_file.read()
+key = base64.b64decode(key)
 
 cipher_suite = Fernet(key)
-chunk_dir = 'path_to_chunks/'
+chunk_dir = 'chunks/'
 output_gz_file = 'reconstructed_file.gz'
 
 def decrypt_and_combine_files(chunk_dir, output_gz_file):
-    part_files = sorted([f for f in os.listdir(chunk_dir) if f.endswith('.gz.enc')])
+    part_files = sorted([f for f in os.listdir(chunk_dir) if f.find('ope_') == 0])
 
     with open(output_gz_file, 'wb') as output_f:
         for part_file in part_files:
             part_path = os.path.join(chunk_dir, part_file)
+            print(f"Processing {part_path}")
             with open(part_path, 'rb') as f:
                 encrypted_chunk = f.read()
 
